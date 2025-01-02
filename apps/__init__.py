2e6b04d5275f8b0215e034369f4f89c72e9507dc
gabeha <gabriel.hauss@gmail.com>
2025-01-02 10:15:38 +0100

    added comments to the __init__.py files

---
+++ apps/__init__.py
@@ -0,0 +1,4 @@
+"""
+For this directory to be treated as a package, it must contain a file named __init__.py.
+This directory contains all the apps that will be used in the project.
+"""
---
+++ backend/__init__.py
@@ -0,0 +1,4 @@
+"""
+The __init__.py file is used to initialize the backend package.
+This app serves as the entry point for the Django project.
+"""
